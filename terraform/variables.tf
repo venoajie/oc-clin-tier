b747ec095d2797d4382b446cbfb6965882f4b62c
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-20 18:10:39 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 
 variable "compartment_ocid" {
-  description = "OCI compartment for HIPAA resources"
+  description = "Target compartment OCID for encryption resources"
   type        = string
   validation {
     condition     = length(var.compartment_ocid) > 4 && substr(var.compartment_ocid, 0, 6) == "ocid1."
@@ -8,11 +8,6 @@ variable "compartment_ocid" {
   }
 }
 
-
-variable "compartment_ocid" {
-  type        = string
-  description = "Target compartment OCID for encryption resources"
-}
 
 variable "env" {
   type        = string
