29ca24634771083dab2b037913c64177fef93de7
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-20 12:50:27 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,9 @@
-variable "compartment_ocid" {
-  type        = string
-  description = "Your OCI compartment OCID"
-}
 
+variable "compartment_ocid" {
+  description = "OCI compartment for HIPAA resources"
+  type        = string
+  validation {
+    condition     = length(var.compartment_ocid) > 4 && substr(var.compartment_ocid, 0, 6) == "ocid1."
+    error_message = "Invalid compartment OCID format."
+  }
+}
